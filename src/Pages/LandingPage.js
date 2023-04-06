9ec52468e2e877981bc5f105256f8fc17b1a72ec
MSmed <mattias.smedman@gmail.com>
2023-04-06 09:59:17 +0200

    feat: Initial Landing Page Styling

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,7 +1,25 @@
+
+
+import {Box} from "@mui/material"
+import Header from "../Components/Header/Header";
+import InfoCard from "../Components/LandingPage/InfoCard";
 const LandingPage = () => {
 
+    const image = <img src="https://w7.pngwing.com/pngs/176/930/png-transparent-catgirl-anime-anime-mammal-cat-like-mammal-carnivoran.png"></img>
+            
     return (
-        <p>Hello!</p>
+        <div style={{backgroundImage: `url("https://media.tenor.com/UvvgjxTnagUAAAAM/cat-meow.gif")`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                width: "100%",
+                height: 1000
+        }}>
+      <Header />
+
+            <Box sx={{width: 1/4, position: 'absolute', right: '20%', top: '30%'}}>
+                <InfoCard></InfoCard>
+            </Box>
+        </div>
     )
 }
 
