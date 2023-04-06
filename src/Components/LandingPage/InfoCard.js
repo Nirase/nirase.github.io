6d81726ecfff338a1ec43daee73377e33269e838
MSmed <mattias.smedman@gmail.com>
2023-04-06 13:35:59 +0200

    feat: Make carousel pretty

--- src/Components/LandingPage/InfoCard.js
+++ src/Components/LandingPage/InfoCard.js
@@ -2,23 +2,22 @@ import { Card, CardContent, Typography, CardActions, Grid, Chip } from "@mui/mat
 import Button from "@mui/material/Button"
 import SellIcon from '@mui/icons-material/Sell';
 import { alpha } from "@mui/material";
-const InfoCard = () => {
+const InfoCard = (props) => {
 
-    const tags = ['Unity', 'C#', 'Github', 'Music', 'Adventure', 'Exploration', 'Atmospheric']
 
     return (
         <Card sx={{ minWidth: 275, backgroundColor: alpha("#ffebee", 0.8)}}>
             <CardContent>
                 <Typography sx={{ mb: 1.5 }} color="text.primary">
-                    Kanvas
+                    {props.name}
                 </Typography>
                 <Typography variant="body2">
-                    Explore an abandoned world in this frozen landscape. Continue moving forward as the only things guiding you is the sound of the animals and the beauty of music.
+                    {props.description}
                 </Typography>
             </CardContent>
 
             <Grid container sx={{mb: 1.5, ml: 1.5}}>
-                {tags.map(x => 
+                {props.tags.map(x => 
                         <Grid item xs={6} sx={{mb: 0.5}}>
                         <Chip
                             label={x}
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,26 +1,83 @@
-
-
-import {Box} from "@mui/material"
+import { Box, Slide } from "@mui/material"
 import Header from "../Components/Header/Header";
 import InfoCard from "../Components/LandingPage/InfoCard";
+import Carousel from 'react-material-ui-carousel'
+import { useState } from "react";
+import "../css/animations.css"
 const LandingPage = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-    const image = <img src="https://w7.pngwing.com/pngs/176/930/png-transparent-catgirl-anime-anime-mammal-cat-like-mammal-carnivoran.png"></img>
-            
+    const handleSlideChange = (index) => {
+        console.log("called", index);
+        setCurrentIndex(index);
+    };
+
+    const items = [
+        {
+            image: "https://media.tenor.com/UvvgjxTnagUAAAAM/cat-meow.gif",
+            name: "Kanvas",
+            description: "Explore an abandoned world in this frozen landscape. Continue moving forward as the only things guiding you is the sound of the animals and the beauty of music.",
+            tags: ['Unity', 'C#', 'Github', 'Music', 'Adventure', 'Exploration', 'Atmospheric'],
+            url: ""
+        }
+
+        ,
+        {
+            image: "https://thumbs.gfycat.com/UnfinishedOccasionalIraniangroundjay-size_restricted.gif",
+            name: "Rougelite",
+            description: "Explore a procedurally generated world filled with unique and amazing monsters. Create the perfect team to fight against the wild enemies you encounter so that you can explore the world.",
+            tags: ['Unity', 'C#', 'Github', 'Procedural Generation', 'RPG', 'Turn Based'],
+            url: ""
+
+        }
+    ];
+    const indicatorStyles = {
+        position: 'absolute',
+        bottom: 0,
+        textAlign: 'center',
+        width: '100%',
+        zIndex: 1,
+    };
+
+    const navButtonStyles = {
+        position: 'absolute',
+        top: '50%',
+        transform: 'translateY(-50%)',
+        zIndex: 1,
+    };
+
+
     return (
-        <div style={{backgroundImage: `url("https://media.tenor.com/UvvgjxTnagUAAAAM/cat-meow.gif")`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                width: "100%",
-                height: 1000
-        }}>
-      <Header />
+        <div style={{ position: 'relative' }}>
+            <Carousel
+                indicatorContainerProps={{ style: indicatorStyles }}
+                navButtonsContainerProps={{ style: navButtonStyles }}
+                onChange={handleSlideChange}
+                animation="slide"
+                duration={800}
+                interval={10000}
+            >
+                {items.map((bg, index) => (
+                    <div key={index}
+                        style={{
+                            backgroundImage: `url("${bg.image}")`,
+                            backgroundRepeat: "no-repeat",
+                            backgroundSize: "cover",
+                            width: "100vw",
+                            height: "100vh",
+                            zIndex: 1
+                        }}></div>
+                ))}
+            </Carousel>
 
-            <Box sx={{width: 1/4, position: 'absolute', right: '20%', top: '30%'}}>
-                <InfoCard></InfoCard>
+
+            <div style={{ position: 'absolute', right: 0, top: 0, left: 0, zIndex: 1 }}>
+                <Header />
+            </div>
+            <Box sx={{ width: 1 / 4, position: 'absolute', right: '20%', top: '30%', zIndex: 1 }}>
+                <InfoCard name={items[currentIndex].name} tags={items[currentIndex].tags} description={items[currentIndex].description} url={items[currentIndex].url} />
             </Box>
         </div>
-    )
-}
-
+    );
+};
 export default LandingPage;
